14394aa45ddff619f282079ab9b9aa649c472656
konie_52 <you524business@gmail.com>
2020-12-25 23:22:41 +0900

    ABC070-B, ABC075-Bを追加

---
+++ BeginnersSelection/ABC070-B/ABC070-B/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC070_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //入力
+            string[] input = Console.ReadLine().Split(' ');
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+            int C = int.Parse(input[2]);
+            int D = int.Parse(input[3]);
+
+            //重なり方によって６つに場合分け
+
+            if(B<=C)
+            {
+                Console.WriteLine(0);
+            }
+            else if(A<=C && C<B && B<=D)
+            {
+                Console.WriteLine(B - C);
+            }
+            else if(A<=C && D<B)
+            {
+                Console.WriteLine(D - C);
+            }
+            else if(C<=A && A<D && D<=B)
+            {
+                Console.WriteLine(D - A);
+            }
+            else if(D<A)
+            {
+                Console.WriteLine(0);
+            }
+            else if(C<=A && B<=D)
+            {
+                Console.WriteLine(B - A);
+            }
+        }
+    }
+}
---
+++ BeginnersSelection/ABC075-B/ABC075-B/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC075_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int H = int.Parse(input[0]);
+            int W = int.Parse(input[1]);
+
+            string[,] math = new string[H, W];
+            for(int i=1;i<=H;i++)
+            {
+                string[,] Si = new string[1, W];
+                Si = Console.ReadLine().Split(' ');
+                for(int j=1;j<=W;j++)
+                {
+
+                }
+            }
+        }
+    }
+}
