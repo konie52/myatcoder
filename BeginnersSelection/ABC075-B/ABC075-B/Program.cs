e8cc62972d0e009cb43ff23671c15abd5213cb75
小西洋太朗 <you524business@gmail.com>
2020-12-30 14:34:33 +0900

    MineSweeperの回答を更新

--- BeginnersSelection/ABC075-B/ABC075-B/Program.cs
+++ BeginnersSelection/ABC075-B/ABC075-B/Program.cs
@@ -10,18 +10,62 @@ namespace ABC075_B
     {
         static void Main(string[] args)
         {
+            //H,Wの入力
             string[] input = Console.ReadLine().Split(' ');
             int H = int.Parse(input[0]);
             int W = int.Parse(input[1]);
 
-            string[,] math = new string[H, W];
-            for(int i=1;i<=H;i++)
-            {
-                string[,] Si = new string[1, W];
-                Si = Console.ReadLine().Split(' ');
-                for(int j=1;j<=W;j++)
-                {
+            //２次配列の定義
+            string[,] Map = new string[H, W];
 
+            //８方向調査のための配列（順番はどうでもOK)
+            int[] dy = new int[8] { 1, 0, -1, 0, 1, -1, -1, 1 };
+            int[] dx=new int[8] { 0, 1, 0, -1, 1, 1, -1, -1 };
+            
+
+            for(int i=0;i<H;i++)
+            {
+                //各行ごとに文字列を読み込む
+                string[] S = Console.ReadLine().Split(' ');
+
+                for(int j=0;j<W;j++)
+                {
+                    //各列の文字を二次配列に格納
+                    Map[i, j] = S[j];
+
+                    if(Map[i,j]=="#")
+                    {
+                        continue;
+                    }
+
+                    //"."だった時に計算
+                    else
+                    {
+                        int num = 0;
+
+                        //８方向調べて"#"があったら+1
+                        for(int d=0;d<8;d++)
+                        {
+                            int ni = i + dy[d];
+                            int nj = j + dx[d];
+
+                            if(ni<0||H<ni)
+                            {
+                                continue;
+                            }
+                            else if(nj<0||W<nj)
+                            {
+                                continue;
+                            }
+                            else if(Map[ni,nj] =="#")
+                            {
+                                num += 1;
+                            }
+                        }
+                        Map[i, j] = num.ToString();
+                    }
+
+                    Console.WriteLine(Map[i, j]);
                 }
             }
         }
